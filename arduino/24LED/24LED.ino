10dcb9131694e5ec78762a9bddaec4135b98fe76
a3510377 <a102009102009@gmail.com>
2023-09-22 20:03:04 +0800

    Update 24LED.ino

--- arduino/24LED/24LED.ino
+++ arduino/24LED/24LED.ino
@@ -14,19 +14,24 @@ enum Mode {
   LEFT_TO_RIGHT,
   LEFT_SPACING_3,
   LEFT_SPACING_4,
+  LEFT_SPACING_2_3,
+  LEFT_CUMULATIVE_3,
+  LEFT_CUMULATIVE_3_LOOP,
 };
 
-uint8_t mode = RIGHT_TO_LEFT;
+uint8_t mode;
+bool modeNodeMode = true;
 // 24       16       8      0
 // 11111111_11111111_11111111
 uint32_t ledCache;
-bool buttonState;
+bool buttonState = false;
 bool lastButtonState = false;
 
 unsigned long lastTime = -PERIOD;
 unsigned long lastDebounceTime;
 
-void update();
+void update(void);
+void setMod(uint8_t mode);
 
 void setup() {
   Serial.begin(9600);
@@ -36,6 +41,8 @@ void setup() {
   pinMode(DATA_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
+
+  setMod(LEFT_SPACING_2_3);
 }
 
 void loop() {
@@ -50,21 +57,9 @@ void loop() {
     buttonState = reading;
 
     if (buttonState) {
-      if (++mode > LEFT_SPACING_4) mode = 0;
+      if (++mode > LEFT_CUMULATIVE_3_LOOP) mode = 0;
 
-      switch (mode) {
-        case RIGHT_TO_LEFT:
-        case LEFT_TO_RIGHT:
-          ledCache = 0;
-          break;
-          // RIGHT_TO_LEFT
-        case LEFT_SPACING_3:
-          ledCache = 0x808080;
-          break;
-        case LEFT_SPACING_4:
-          ledCache = 0x20820;
-          break;
-      }
+      setMod(mode);
 
       Serial.println("--------------------");
     }
@@ -74,28 +69,48 @@ void loop() {
   if (nowTime >= lastTime + PERIOD) {
     lastTime = nowTime;
     switch (mode) {
+      case LEFT_TO_RIGHT:
+      case LEFT_SPACING_3:
+      case LEFT_SPACING_4:
+      case LEFT_SPACING_2_3:
+        ledCache <<= 1;
+        if (ledCache >> 24 || ledCache <= 0) {
+          ledCache &= 0xffffff;
+          ledCache |= 1;
+        }
+        break;
       case RIGHT_TO_LEFT:
         ledCache >>= 1;
         if (ledCache <= 0) {
           ledCache = 0x800000;  // 0b100000000000000000000000
         }
         break;
-      case LEFT_TO_RIGHT:
+      case LEFT_CUMULATIVE_3:
         ledCache <<= 1;
-        if (ledCache > 0xffffff || ledCache <= 0) {
-          ledCache = 1;
+        ledCache |= 0x10101;
+        if (ledCache >> 24) {
+          ledCache = 0x10101;
         }
         break;
-      case LEFT_SPACING_3:
-        ledCache <<= 1;
-        if (!(ledCache & 0xff)) {
-          ledCache |= 1;
-        }
-        break;
-      case LEFT_SPACING_4:
-        ledCache <<= 1;
-        if (!(ledCache & 0x3f)) {
-          ledCache |= 1;
+      case LEFT_CUMULATIVE_3_LOOP:
+        if (modeNodeMode) {
+          ledCache <<= 1;
+          ledCache |= 0x10101;
+          if (ledCache >> 24) {
+            modeNodeMode = false;
+          }
+        } else {
+          uint8_t tmp = ledCache & ((ledCache & 0xff) >> 1);
+
+          ledCache = tmp;
+          for (uint8_t i = 0; i < 2; i++) {
+            ledCache <<= 8;
+            ledCache |= tmp;
+          }
+          if (ledCache <= 0) {
+            ledCache = 0x10101;
+            modeNodeMode = true;
+          }
         }
         break;
     }
@@ -116,4 +131,30 @@ void update() {
   }
   digitalWrite(LATCH_PIN, HIGH);
   Serial.println("--------");
+}
+
+void setMod(uint8_t _mode) {
+  switch (_mode) {
+    case RIGHT_TO_LEFT:
+    case LEFT_TO_RIGHT:
+      ledCache = 0;
+      break;
+      // RIGHT_TO_LEFT
+    case LEFT_SPACING_4:
+      ledCache = 0x820820;
+      break;
+    case LEFT_SPACING_2_3:
+      ledCache = 0xe00e00;
+      break;
+    case LEFT_SPACING_3:
+    case LEFT_CUMULATIVE_3:
+    case LEFT_CUMULATIVE_3_LOOP:
+      ledCache = 0x10101;
+      break;
+  }
+
+  mode = _mode;
+  modeNodeMode = true;
+  Serial.print("setMode: ");
+  Serial.println(mode);
 }
